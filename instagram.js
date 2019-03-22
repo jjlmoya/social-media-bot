e47bca9452c447adf426a2262a9fc159b74794e8
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-03-22 14:26:15 +0100

    Add Credentials Service

--- instagram.js
+++ instagram.js
@@ -1,13 +1,14 @@
-const Driver = rnode equire('./configuration/driver');
+const Driver = require('./configuration/driver');
 const Selenium = require('selenium-webdriver');
 const Load = require('./configuration/loadModule');
+const Credentials = require('./configuration/credentials');
 const Instagram = Load('../service/instagramService', {
     driver: Driver,
     selenium: Selenium
 });
 (function () {
     var init = function () {
-        Instagram.login('bonseo.es', '');
+        Instagram.login(Credentials.instagram.name, Credentials.instagram.password);
     };
     init();
 })();
--- service/instagramService.js
+++ service/instagramService.js
@@ -20,5 +20,27 @@ module.exports = {
         driver.wait(selenium.until.elementLocated(selenium.By.className('GAMXX')), 5 * 1000).then(function (el) {
             return el.click();
         });
-    }
+    },
+    exploreHashTag: function (hashtag) {
+        /**
+         ¿Estoy logueado? Logueate
+         Pagina inicio? IR
+         Click en la Lupa
+         Click en Input Buscar
+         Escribir hashtag (añadir almohadilla)
+         Pulsar sobre los X primeros
+             Al pulsar hacer:
+                IR RECIENTES
+                Las Y primeras publicaciones
+                        Pulsar publicación
+                        (Like al Post)
+                        IR a me gusta
+                        Seguir a todos
+                        Volver Atrás (Publicaciones modo lista)
+                Volver Atrás (Publicaciones)
+            Volver atrás (Lista de hashtags)
+         Volver atrás (Página inicio)
+         * **/
+    },
+
 };
